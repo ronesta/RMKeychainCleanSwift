2bcda2758569e02a874a73cdd31e242d4cb8297f
ronesta <gvozzzd123@gmail.com>
2025-02-12 15:30:26 +0300

    First iteration of Clean Swift architecrure

---
+++ RMKeychainCleanSwift/RMKeychainCleanSwift/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  RMKeychainCleanSwift
+//
+//  Created by Ибрагим Габибли on 11.02.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
